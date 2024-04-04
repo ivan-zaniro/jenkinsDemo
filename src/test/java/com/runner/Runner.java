7eabcab0f3db31bcbfab64005fdd2b3d34252fee
ivan-zaniro <155381222+ivan-zaniro@users.noreply.github.com>
2024-04-04 06:47:30 +0300

    Update Runner.java
    added' tags = "@ivan"'

--- src/test/java/com/runner/Runner.java
+++ src/test/java/com/runner/Runner.java
@@ -14,6 +14,7 @@ import io.cucumber.junit.CucumberOptions;
 @CucumberOptions (
 					features = "src/test/resources/smoke/SmokeTest.feature",
 					glue = "com.steps",
+					tags = "@ivan"
 					plugin = {"pretty",
 					"json:target/cucumber.json",
 					"html:target/cucumber.html"}
